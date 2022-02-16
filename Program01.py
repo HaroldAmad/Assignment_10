775f92dded9881c2ead71f6ec4edb9a51d38c380
HaroldAmad <harold.amad@gmail.com>
2022-02-16 18:06:42 +0800

    Initiatin Webcam for VideoCapture

--- Program01.py
+++ Program01.py
@@ -3,3 +3,12 @@ import numpy as np
 from pyzbar.pyzbar import decode
 import datetime
 import pytz
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    decoder(frame)
+    cv2.imshow('Image', frame)
+    code = cv2.waitKey(10)
+    if code == ord('q'):
+        break
