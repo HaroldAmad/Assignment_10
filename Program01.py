07a6ee6658424685d7a7ad06c00c41b9fec0812e
HaroldAmad <harold.amad@gmail.com>
2022-02-16 18:09:01 +0800

    QRcode Decoder Function

--- Program01.py
+++ Program01.py
@@ -4,6 +4,23 @@ from pyzbar.pyzbar import decode
 import datetime
 import pytz
 
+def decoder(image):
+    gray_img = cv2.cvtColor(image,0)
+    QRcode = decode(gray_img)
+
+    for obj in QRcode:
+        points = obj.polygon
+        (x,y,w,h) = obj.rect
+        pts = np.array(points, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(image, [pts], True, (0, 255, 0), 3)
+
+        QRcodeInfo = obj.data.decode("utf-8")
+        string = "Data " + str(QRcodeInfo)
+        
+        cv2.putText(frame, string, (x,y), cv2.FONT_ITALIC,0.8,(0,255,0), 2)
+        print("Data: "+QRcodeInfo+"")
+
 cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
